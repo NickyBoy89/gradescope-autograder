c1e353d5adf9478e8ec93f70311d4f5bd8ee1ec0
Nicholas Novak <nicholasmnovak@gmail.com>
2021-11-08 17:20:49 -0700

    Minor refactors, updated readme, and fixed directory hacks

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Gradescope-Autograder
 
-Gradescope autograder is a program that runs the Gradescope testcases against your own code, and instead of diff messages such as "these two lines are different", where there is an uncertain number of invisible characters different between the two lines
+Gradescope autograder is a program that runs python Gradescope testcases against your own code
+
+It provides configurable diff messages, and shows invisible characters that would otherwise cause errors without a clear cause
 
 ## Requirements
 
@@ -10,14 +12,9 @@ Gradescope autograder is a program that runs the Gradescope testcases against yo
 
 1. Build the binary with `go build`
 
-2. Copy your code into the same directory that the tests are in
-    * For Linux/MacOS, just append a "`cp *.py <directory that tests are in> && `" to the beginning of the next code
-
-3. Run the binary with `./gradescope-autograder --main <your code file> --targetDir <directory that the tests are in>`
-
-4. If you have any errors, make sure that you copied your code files into the directory that the test files are in
+2. Run the binary with `./gradescope-autograder --main <your code file> --targetDir <directory that the tests are in>`
 
 ## Command-line args
 
-* `--main` specifies the code to test against, defaults to `main.py`
+* `--main` specifies the code to test against, required for testcases that run a python program specifically
 * `--targetDir` specifies the directory that you put the tests in, defaults to `.`, or the current directory
--- autograder.go
+++ autograder.go
@@ -27,7 +27,7 @@ const commandTimeout = time.Second * 1
 // Also takes in files beginning with ".stdin" which test the execution of the entire program
 
 func main() {
-	mainFile := flag.String("main", "main.py", "The python file that contains the main function, defaults to \"main.py\"")
+	mainFile := flag.String("main", "", "The python file to run the tests against, required for programs that test that file specifically")
 	targetDirectory := flag.String("targetDir", ".", "The directory to search for test files in, defaults to \".\" (the current directory)")
 	stopFail := flag.Bool("stopFail", false, "Stops the test after the first test failure. Useful with large test diffs")
 	verbose := flag.Bool("v", false, "Always prints the stdout of commands, instead of only on error")
@@ -35,27 +35,19 @@ func main() {
 
 	flag.Parse()
 
-	if *mainFile == "main.py" {
-		log.Warn("No main python file specified, defaulting to \"main.py\". Specify a main file with --main <filename>")
-	}
-
 	if *targetDirectory == "." {
 		log.Warn("No target directory specified, the program will look for tests in the current directory")
 		log.Warn("If this is not what you intended, set a target directory with the --targetDir flag")
 	}
 
-	err := os.Chdir(*targetDirectory)
-	if err != nil {
-		log.Fatal("Error changing directory: %v", err)
-	}
-
-	// Get all the matching python files to run as tests
-	testFiles, err := filepath.Glob("test-*.py")
+	// Gather all the test files
+	testFiles, err := filepath.Glob(*targetDirectory + "/test-*.py")
 	if err != nil {
 		log.Fatalf("Error searching for test files: %v", err)
 	}
 
-	// Run all the tests that test parts of the program
+	// Run the testcases that just compare their outputs
+	// but do not run a program directly
 	color.Blue("Running tests now")
 	for _, test := range testFiles {
 		fmt.Printf("Running test [%v] ... ", color.GreenString(test))
@@ -70,33 +62,26 @@ func main() {
 			log.Fatalf("Error opening output file %v: %v", ChangeFileExtensionTo(test, ".out"), err)
 		}
 
-		if string(output) != string(testOutput) {
-			color.Red("FAILED")
-
-			if !*raw {
-				diff := diffmatchpatch.New()
-				outputDifference := diff.DiffMain(string(output), string(testOutput), true)
-				fmt.Println(diff.DiffPrettyText(outputDifference))
-			} else {
-				color.Blue("EXPECTED")
-				fmt.Println(string(testOutput))
-				color.Blue("ACTUAL")
-				fmt.Println(string(output))
-			}
-			if *stopFail {
-				break
-			}
-			continue
+		if !compareOutputs(string(output), string(testOutput), *raw) && *stopFail {
+			break
 		}
-		color.Green("PASSED")
 	}
 
 	// Get all the .stdin files
-	inFiles, err := filepath.Glob("*.stdin")
+	inFiles, err := filepath.Glob(*targetDirectory + "/*.stdin")
 	if err != nil {
 		log.Fatalf("Error searching for .stdin files: %v", err)
 	}
 
+	// Make sure that the user didn't leave the program name blank
+	// and there were testcases to run against them
+	if *mainFile == "" && len(inFiles) > 0 {
+		log.Error("A main python file was not specified, but was required for some testcases")
+		log.Error("Please specify one with the --main flag")
+		return
+	}
+
+	// Run all the testcases that call a python program directly
 	color.Blue("Comparing output of program to testcases")
 	for _, testInput := range inFiles {
 		testOutput := ChangeFileExtensionTo(testInput, ".out")
@@ -129,29 +114,33 @@ func main() {
 			log.Fatalf("Error reading output file %v: %v", testOutput, err)
 		}
 
-		if string(commandOutput) != string(outputData) {
-			color.Red("FAILED")
-			if !*raw {
-				// Find the diff between the outputs of the two strings
-				diff := diffmatchpatch.New()
-				outputDifference := diff.DiffMain(string(outputData), string(commandOutput), false)
-				//err := os.WriteFile("index.html", []byte("<html><body style=\"white-space:pre;\">"+diff.DiffPrettyHtml(outputDifference)+"</body></html>"), 0755)
-				//if err != nil {
-				//	panic(err)
-				//}
-				fmt.Println(diff.DiffPrettyText(outputDifference))
-			} else {
-				color.Blue("EXPECTED")
-				fmt.Println(string(outputData))
-				color.Blue("ACTUAL")
-				fmt.Println(string(commandOutput))
-			}
-			if *stopFail {
-				break
-			}
+		if !compareOutputs(string(commandOutput), string(outputData), *raw) && *stopFail {
+			break
 		}
-		color.Green("PASSED")
 	}
+}
+
+// Compares the outputs of two commands, and prints the diff of them
+// if the raw arg is passed, it instead prints out the raw outputs of the two commands
+// The function also returns the status of the test executed within it
+func compareOutputs(expected, actual string, raw bool) bool {
+	if actual != expected {
+		color.Red("FAILED")
+		if !raw {
+			// Find the diff between the outputs of the two strings
+			diff := diffmatchpatch.New()
+			outputDifference := diff.DiffMain(expected, actual, false)
+			fmt.Println(diff.DiffPrettyText(outputDifference))
+		} else {
+			color.Blue("EXPECTED")
+			fmt.Println(expected)
+			color.Blue("ACTUAL")
+			fmt.Println(actual)
+		}
+		return false
+	}
+	color.Green("PASSED")
+	return true
 }
 
 func ChangeFileExtensionTo(path, ext string) string {
